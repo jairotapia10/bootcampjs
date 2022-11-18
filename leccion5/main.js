0324c5e32e0a93765e0986629692ea097526bc7d
jairotapia10 <jaitap10@gmail.com>
2022-11-17 23:38:53 -0500

    leccionfunciones

---
+++ leccion5/main.js
@@ -0,0 +1,18 @@
+/*function sumar (objeto) {
+    console.log(objeto.num1+objeto.num2+objeto.num3);
+}
+
+const numerobjeto = {
+    num1: 2,
+    num2:3,
+    num3: 5
+}
+
+sumar(numerobjeto);
+*/
+
+arreglo = [1,2,3,4,6];
+
+arreglofuncion = arreglo.filter ((numero) => numero % 2 === 0);
+
+console.log(arreglofuncion);
